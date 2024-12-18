16997548a9640cefdf3a19e9b4018c6d7aff64e3
Park Sugwan <456tnrhks@naver.com>
2024-12-19 01:25:46 +0900

    삭제수정

--- scr/event.js
+++ scr/event.js
@@ -24,17 +24,18 @@ document.addEventListener("DOMContentLoaded", async () => {
         method: "DELETE",
         headers: HEADERS,
       });
-
+  
       if (!response.ok) {
         throw new Error(`문서 삭제 실패 (ID: ${docId})`);
       }
-
+  
       console.log("문서 삭제 성공");
       await renderSidebar();
-
-      // 문서가 삭제 될때 main textarea 도 같이 삭제 되게 
+  
+       // 문서가 삭제 될때 main textarea 도 같이 삭제 되게 
       const titleBox = document.querySelector(".main h2");
       if (titleBox && titleBox.dataset.id === docId) {
+        const contentArea = document.querySelector(".main");
         contentArea.innerHTML = `
           <div class="welcome_box">
             <p>문서를 선택하거나 새 문서를 생성하세요.</p>
@@ -45,7 +46,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       console.error("문서 삭제 중 오류 발생:", error);
     }
   };
-
+  
   // 문서 내용 렌더링
   const renderEditor = async (docId) => {
     const currentDoc = await fetchDocumentById(docId);
@@ -109,8 +110,8 @@ document.addEventListener("DOMContentLoaded", async () => {
   });
 
   // 사이드바 클릭 시 해당 문서 내용 렌더링
-  document.querySelector(".menu ul").addEventListener("click", (event) => {
-    const menuBox = event.target.closest(".menu_box");
+  document.querySelector('.menu ul').addEventListener('click', (event) => {
+    const menuBox = event.target.closest('.menu_box');
     if (menuBox) {
       const docId = menuBox.dataset.id;
       if (docId) {
